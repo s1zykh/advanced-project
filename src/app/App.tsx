67de40a15a87138508fcfb7cd986d37e74eae927
s1zykh <vladik.sizykh21@mail.ru>
2024-03-19 21:40:29 +0300

    The configuration for the router has been added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
-import React, { Suspense, useContext, useState } from "react";
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
 import { useTheme } from "app/providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
 
-import "styles/index.scss";
+import "./styles/index.scss";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -15,12 +12,7 @@ const App = () => {
       <button onClick={toggleTheme}>ТЕМА</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
