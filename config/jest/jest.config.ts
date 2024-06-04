1ad0733dc5d3d9052f034547c052eb1e1fee9a8b
s1zykh <vladik.sizykh21@mail.ru>
2024-06-04 13:47:34 +0300

    add raect testing library, add tests for btn, sidebar

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -4,6 +4,7 @@
  */
 
 import type { Config } from "jest";
+import path from "path";
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -32,6 +33,12 @@ const config: Config = {
   ],
   testMatch: ["<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)"],
   rootDir: "../../",
+  modulePaths: ["<rootDir>src"],
+  setupFilesAfterEnv: ["<rootDir>config/jest/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.(s?css)$": "identity-obj-proxy",
+    "\\.svg": path.resolve(__dirname, "jestEmptyComponent.tsx"),
+  },
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
