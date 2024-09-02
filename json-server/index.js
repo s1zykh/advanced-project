c84f66eb1f062972bc162b62025a474c0f1d6acb
s1zykh <vladik.sizykh21@mail.ru>
2024-09-02 18:16:05 +0300

    add https for json server

--- json-server/index.js
+++ json-server/index.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
+const https = require('https');
 
+const options = {
+    key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+};
 const server = jsonServer.create();
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 server.use(jsonServer.defaults({}));
@@ -17,10 +22,14 @@ server.use(async (req, res, next) => {
 server.post('/login', (req, res) => {
     try {
         const { username, password } = req.body;
-        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const db = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'),
+        );
         const { users = [] } = db;
 
-        const userFromBd = users.find((user) => user.username === username && user.password === password);
+        const userFromBd = users.find(
+            (user) => user.username === username && user.password === password,
+        );
         if (userFromBd) {
             return res.json(userFromBd);
         }
@@ -42,6 +51,8 @@ server.use((req, res, next) => {
 
 server.use(router);
 
-server.listen(8000, () => {
+const httpsServer = https.createServer(options, server);
+
+server.listen(443, () => {
     console.log('server is running on 8000 port');
 });
